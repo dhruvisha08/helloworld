e071e2099607ef8c924c05ec3b040b656d9f219e
dhruvisha08 <dhruvishab@gmail.com>
2021-09-07 18:40:30 +0530

    Updates

---
+++ static/js/main.d648ba1b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonphello=this.webpackJsonphello||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),i=c.n(s),o=c(0);var h=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"BTS"})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(h,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.d648ba1b.chunk.js.map
