6284eeec7f0c3c004cca6bed52897849c2401ccf
dhruvisha08 <dhruvishab@gmail.com>
2021-09-07 18:51:48 +0530

    commitR

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import AddClient from './AddClient';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-
-function App() {
-  return (
-    <div className="App">
-    <h1>BTS</h1>
-    </div>
-  );
+const App = () => {
+    return(
+        <div>
+            <Router>
+            <Switch>
+                <Route path="/" exact component={SignUp}/>
+                <Route path="/signUp" component={SignUp}/>
+                <Route path="/signIn" component={SignIn}/>
+                <Route path="/addClient" component={AddClient}/>
+            </Switch>
+            </Router>
+        </div>
+        
+    )
 }
 
 export default App;
